0f6099ac53c13323cccd7f53db21306230507be7
AnnA <anna_elfimova@mail.ru>
2022-07-12 14:01:54 +0300

    chetniye chisla ot 1 do N

---
+++ task08/Program.cs
@@ -0,0 +1,13 @@
+﻿// chetniye chisla ot 1 do N
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int vrem = 1;
+while (vrem <= N)
+{
+    if (vrem % 2 == 0)
+    {
+        Console.Write(vrem + " ");
+    }
+    vrem++;
+}
