73da0624a807d9174548ab82c99bf1c12a1ab263
AnnA <anna_elfimova@mail.ru>
2022-07-12 13:03:54 +0300

    max iz tryoh chisel

---
+++ task04/Program.cs
@@ -0,0 +1,46 @@
+﻿// max iz tryoh chisel
+
+int a, b, c;
+Console.WriteLine("Введите три числа : : ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+
+
+int max;
+
+if (a >= b)
+ {
+    if (c >= a)
+    {
+       max = c;
+    }
+    else
+    {
+       max = a;
+    }
+ }
+else
+ {
+    if (c >= b)
+    {
+       max = c;
+    }
+    else
+    {
+       max = b;
+    }
+ }
+Console.WriteLine(max);
+
+
+
+
+
+
+
+
+
+
+
+
