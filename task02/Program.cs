59caed381fc334e7d823c2de2b670032d7bd27c6
AnnA <anna_elfimova@mail.ru>
2022-07-12 11:33:31 +0300

    task 2 <> dva chisla

---
+++ task02/Program.cs
@@ -0,0 +1,28 @@
+﻿// <> dva chisla
+
+int a, b;
+Console.WriteLine("Введите два числа : ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+ 
+if (a > b)
+ {
+ Console.WriteLine("{0} - бOльшее число", a);
+ }
+else
+ {
+ Console.WriteLine("{0} - бOльшее число ", b);
+ }
+ 
+Console.ReadLine();
+
+if (a < b)
+{
+Console.WriteLine("{0} - меньшее число", a);
+}
+else
+{
+Console.WriteLine("{0} - меньшее число ", b);
+}
+
+Console.ReadLine();
