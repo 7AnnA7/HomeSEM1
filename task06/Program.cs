f58c15723dda5878ce2ffe2a9820a3c88a6b1639
AnnA <anna_elfimova@mail.ru>
2022-07-12 13:15:37 +0300

    chetnoe li chislo

---
+++ task06/Program.cs
@@ -0,0 +1,15 @@
+﻿// chetnoe li chislo
+
+int a;
+Console.WriteLine("Введите число: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+if (a%2==0)
+{
+Console.WriteLine("Это чётное число");
+}
+
+else
+{
+Console.WriteLine("Это нечётное число");
+}
